a427e0d5e381de0c096298d3990bb87c131fddb6
Antra Tessera <antra@tesserae.cc>
2024-12-20 23:01:51 -0500

    tesgo

--- worker_train.sh
+++ worker_train.sh
@@ -15,7 +15,7 @@ echo "HF_TOKEN is${HF_TOKEN:+ set}${HF_TOKEN:-" not set"}"
 cd ~/EasyLM && python -m EasyLM.models.llama.llama_train \
     --mesh_dim='-1,8,1' \
     --llama.base_model='llama3_8b' \
-    --tokenizer="meta-llama/Meta-Llama-3-8B"
+    #--tokenizer="meta-llama/Meta-Llama-3-8B"
     --load_checkpoint='params::gs://finetune70b/llama-3-8b/llama-3-8b' \
     --train_dataset.type='huggingface' \
     --train_dataset.text_processor.fields='[instruction+input],output' \
