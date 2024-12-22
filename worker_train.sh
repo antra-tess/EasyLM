41ad6695fa91a8e28cab67a1ebd85dacaf941b70
Your Name <you@example.com>
2024-12-22 01:07:54 +0000

    t

--- worker_train.sh
+++ worker_train.sh
@@ -10,28 +10,13 @@ sudo mkdir -p /tmp/tpu_logs && sudo chown -R $USER:$USER /tmp/tpu_logs && sudo c
 echo "WANDB_API_KEY is${WANDB_API_KEY:+ set}${WANDB_API_KEY:-" not set"}"
 echo "HF_TOKEN is${HF_TOKEN:+ set}${HF_TOKEN:-" not set"}"
 
-#
-## Run training
-#cd ~/EasyLM && python -m EasyLM.models.llama.llama_train \
-#    --mesh_dim='-1,8,1' \
-#    --llama.base_model='llama3_8b' \
-#    --tokenizer="meta-llama/Meta-Llama-3-8B" \
-#    --load_checkpoint='params::gs://finetune70b/llama-3-8b/llama-3-8b' \
-#    --train_dataset.type='huggingface' \
-#    --train_dataset.text_processor.fields='[instruction+input],output' \
-#    --train_dataset.huggingface_dataset.name="" \
-#    --train_dataset.huggingface_dataset.path='tatsu-lab/alpaca' \
-#    --logger.online=true \
-#    --logger.project='levanter-sft' \
-#    --logger.entity='antra-cyborgism'
-
 ## Run training
 cd ~/EasyLM && python -m EasyLM.models.llama.llama_train \
     --mesh_dim='-1,8,1' \
     --dtype='bf16' \
     --llama.base_model='llama3_8b' \
-    --tokenizer="meta-llama/Meta-Llama-3-8B" \
-    --load_checkpoint='params::gs://finetune70b/llama-3-8b/llama-3-8b' \
+    --tokenizer="meta-llama/Meta-Llama-3.1-8B" \
+    --load_checkpoint='params::gs://finetune70b/llama-3.1-8b' \
     --train_dataset.type='huggingface' \
     --train_dataset.text_processor.fields='[instruction+input],output' \
     --train_dataset.huggingface_dataset.name="" \
@@ -49,4 +34,4 @@ cd ~/EasyLM && python -m EasyLM.models.llama.llama_train \
     --logger.online=true \
     --logger.project='levanter-sft' \
     --logger.entity='antra-cyborgism' \
-    --logger.output_dir='gs://finetune70b/easylm_checkpoints'
+    --logger.output_dir='gs://finetune70b/easylm_checkpoints2'
