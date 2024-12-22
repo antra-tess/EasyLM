ecf23fc1f9c987fbc220fc01c584643f32b6da2b
Your Name <you@example.com>
2024-12-22 01:09:55 +0000

    te

--- train_tpu_pod.sh
+++ train_tpu_pod.sh
@@ -12,6 +12,6 @@ git push antra
 # Copy worker script and start training on all workers
 echo "Starting training on all workers..."
 gcloud compute tpus tpu-vm scp worker_train.sh finetune-70b:~/worker_train.sh --zone=us-central2-b --worker=all
-gcloud compute tpus tpu-vm ssh finetune-70b --zone=us-central2-b --worker=all --command="cd ~/EasyLM && git pull && chmod +x ~/worker_train.sh && export WANDB_API_KEY='${WANDB_API_KEY}'  && export HF_TOKEN='${HF_TOKEN}' && ~/worker_train.sh"
+gcloud compute tpus tpu-vm ssh finetune-70b --zone=us-central2-b --worker=all --command="cd ~/EasyLM && git reset --hard HEAD && git pull && chmod +x ~/worker_train.sh && export WANDB_API_KEY='${WANDB_API_KEY}'  && export HF_TOKEN='${HF_TOKEN}' && ~/worker_train.sh"
 
 echo "Training deployment complete!"
