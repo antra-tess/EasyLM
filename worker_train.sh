d952d1e3c5c8af233aa76f4ecbd3a185abf9af53
Your Name <you@example.com>
2024-12-30 18:56:52 +0000

    aqsd

--- worker_train.sh
+++ worker_train.sh
@@ -21,7 +21,7 @@ cd ~/EasyLM && python -m EasyLM.models.llama.llama_train \
     --train_dataset.text_processor.fields='[instruction+input],output' \
     --train_dataset.huggingface_dataset.name="" \
     --train_dataset.huggingface_dataset.path='tatsu-lab/alpaca' \
-    --train_dataset.huggingface_dataset.seq_length=1024 \
+    --train_dataset.huggingface_dataset.seq_length=4096 \
     --train_dataset.huggingface_dataset.batch_size=64 \
     --optimizer.type='adamw' \
     --optimizer.adamw_optimizer.lr=3e-4 \
