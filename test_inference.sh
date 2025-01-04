85c8dabf7df4f77596721711eed43bf3d29762fc
Your Name <you@example.com>
2025-01-04 20:16:06 +0000

    test

--- test_inference.sh
+++ test_inference.sh
@@ -12,7 +12,7 @@ echo "HF_TOKEN is${HF_TOKEN:+ set}${HF_TOKEN:-" not set"}"
 cd ~/EasyLM
 
 # Run test inference
-python -m EasyLM.test_inference
+#python -m EasyLM.test_inference
 #!/bin/bash
 
 set -eu
