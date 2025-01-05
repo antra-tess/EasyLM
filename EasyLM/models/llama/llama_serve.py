aaab2202750b1665f2dc4b8ad8adda73d08e0100
Antra Tessera <antra@tesserae.cc>
2025-01-04 22:38:55 -0500

    mesh change

--- EasyLM/models/llama/llama_serve.py
+++ EasyLM/models/llama/llama_serve.py
@@ -211,96 +211,95 @@ class ModelServer(LMServer):
         #     base_model_ps, get_float_dtype_by_name(FLAGS.param_dtype)
         # )
 
-        @partial(
-            pjit,
-            in_shardings=(combined_ps, PS(), PS()),
-            out_shardings=(PS(), PS(), PS())
-        )
-        def forward_loglikelihood(params, rng, batch):
-            batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
-            rng_generator = JaxRNG(rng)
-            input_tokens = batch['input_tokens']
-            output_tokens = batch['output_tokens']
-            input_mask = batch['input_mask']
-            output_mask = batch['output_mask']
-
-            logits = hf_model.module.apply(
-                params, input_tokens, attention_mask=input_mask,
-                deterministic=True, rngs=rng_generator(LLaMAConfigurator.rng_keys()),
-            ).logits
-            loglikelihood = -optax.softmax_cross_entropy_with_integer_labels(
-                logits, output_tokens
+            @partial(
+                pjit,
+                in_shardings=(combined_ps, PS(), PS()),
+                out_shardings=(PS(), PS(), PS())
             )
-            loglikelihood = jnp.sum(loglikelihood * output_mask, axis=-1)
-            match_count = jnp.sum(
-                (jnp.argmax(logits, axis=-1) == output_tokens) * output_mask,
-                axis=-1
+            def forward_loglikelihood(params, rng, batch):
+                batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
+                rng_generator = JaxRNG(rng)
+                input_tokens = batch['input_tokens']
+                output_tokens = batch['output_tokens']
+                input_mask = batch['input_mask']
+                output_mask = batch['output_mask']
+
+                logits = hf_model.module.apply(
+                    params, input_tokens, attention_mask=input_mask,
+                    deterministic=True, rngs=rng_generator(LLaMAConfigurator.rng_keys()),
+                ).logits
+                loglikelihood = -optax.softmax_cross_entropy_with_integer_labels(
+                    logits, output_tokens
+                )
+                loglikelihood = jnp.sum(loglikelihood * output_mask, axis=-1)
+                match_count = jnp.sum(
+                    (jnp.argmax(logits, axis=-1) == output_tokens) * output_mask,
+                    axis=-1
+                )
+                total = jnp.sum(output_mask, axis=-1)
+                is_greedy = match_count == total
+                return loglikelihood, is_greedy, rng_generator()
+
+            self.forward_loglikelihood = forward_loglikelihood
+
+            @partial(
+                pjit,
+                in_shardings=(combined_ps, PS(), PS(), PS()),
+                out_shardings=(PS(), PS())
             )
-            total = jnp.sum(output_mask, axis=-1)
-            is_greedy = match_count == total
-            return loglikelihood, is_greedy, rng_generator()
+            def forward_generate(params, rng, batch, temperature):
+                batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
+                rng_generator = JaxRNG(rng)
+                jax.debug.print("Starting actual forward pass at {}", jax.process_index())
+                output = hf_model.generate(
+                    batch['input_tokens'],
+                    attention_mask=batch['attention_mask'],
+                    params=params['params'],
+                    prng_key=rng_generator(),
+                    logits_processor=FlaxLogitsProcessorList(
+                        [FlaxTemperatureLogitsWarper(temperature)]
+                    ),
+                    generation_config=GenerationConfig(
+                        max_new_tokens=FLAGS.seq_length - FLAGS.input_length,
+                        pad_token_id=self.tokenizer.eos_token_id,
+                        bos_token_id=self.tokenizer.bos_token_id,
+                        eos_token_id=self.tokenizer.eos_token_id,
+                        do_sample=FLAGS.do_sample,
+                        num_beams=FLAGS.num_beams,
+                        top_k=FLAGS.top_k,
+                        top_p=FLAGS.top_p,
+                    )
+                ).sequences[:, batch['input_tokens'].shape[1]:]
+                return output, rng_generator()
 
-        self.forward_loglikelihood = forward_loglikelihood
+            self.forward_generate = forward_generate
 
-        @partial(
-            pjit,
-            in_shardings=(combined_ps, PS(), PS(), PS()),
-            out_shardings=(PS(), PS())
-        )
-        def forward_generate(params, rng, batch, temperature):
-            batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
-            rng_generator = JaxRNG(rng)
-            jax.debug.print("Starting actual forward pass at {}", jax.process_index())
-            output = hf_model.generate(
-                batch['input_tokens'],
-                attention_mask=batch['attention_mask'],
-                params=params['params'],
-                prng_key=rng_generator(),
-                logits_processor=FlaxLogitsProcessorList(
-                    [FlaxTemperatureLogitsWarper(temperature)]
-                ),
-                generation_config=GenerationConfig(
-                    max_new_tokens=FLAGS.seq_length - FLAGS.input_length,
-                    pad_token_id=self.tokenizer.eos_token_id,
-                    bos_token_id=self.tokenizer.bos_token_id,
-                    eos_token_id=self.tokenizer.eos_token_id,
-                    do_sample=FLAGS.do_sample,
-                    num_beams=FLAGS.num_beams,
-                    top_k=FLAGS.top_k,
-                    top_p=FLAGS.top_p,
-                )
-            ).sequences[:, batch['input_tokens'].shape[1]:]
-            return output, rng_generator()
+            @partial(
+                pjit,
+                in_shardings=(combined_ps, PS(), PS()),
+                out_shardings=(PS(), PS())
+            )
+            def forward_greedy_generate(params, rng, batch):
+                batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
+                rng_generator = JaxRNG(rng)
+                output = hf_model.generate(
+                    batch['input_tokens'],
+                    attention_mask=batch['attention_mask'],
+                    params=params['params'],
+                    prng_key=rng_generator(),
+                    generation_config=GenerationConfig(
+                        max_new_tokens=FLAGS.seq_length - FLAGS.input_length,
+                        pad_token_id=self.tokenizer.eos_token_id,
+                        bos_token_id=self.tokenizer.bos_token_id,
+                        eos_token_id=self.tokenizer.eos_token_id,
+                        do_sample=False,
+                        num_beams=1,
+                    )
+                ).sequences[:, batch['input_tokens'].shape[1]:]
+                return output, rng_generator()
 
-        self.forward_generate = forward_generate
+            self.forward_greedy_generate = forward_greedy_generate
 
-        @partial(
-            pjit,
-            in_shardings=(combined_ps, PS(), PS()),
-            out_shardings=(PS(), PS())
-        )
-        def forward_greedy_generate(params, rng, batch):
-            batch = with_sharding_constraint(batch, PS(('dp', 'fsdp')))
-            rng_generator = JaxRNG(rng)
-            output = hf_model.generate(
-                batch['input_tokens'],
-                attention_mask=batch['attention_mask'],
-                params=params['params'],
-                prng_key=rng_generator(),
-                generation_config=GenerationConfig(
-                    max_new_tokens=FLAGS.seq_length - FLAGS.input_length,
-                    pad_token_id=self.tokenizer.eos_token_id,
-                    bos_token_id=self.tokenizer.bos_token_id,
-                    eos_token_id=self.tokenizer.eos_token_id,
-                    do_sample=False,
-                    num_beams=1,
-                )
-            ).sequences[:, batch['input_tokens'].shape[1]:]
-            return output, rng_generator()
-
-        self.forward_greedy_generate = forward_greedy_generate
-
-        with self.mesh:
             logging.info("Sharding parameters across mesh...")
             # Get combined sharding functions for both base and LoRA parameters
 
