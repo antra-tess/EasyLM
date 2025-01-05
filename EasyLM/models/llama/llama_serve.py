38b027c21f23eaea781168ff452f67c0fe3bd033
Antra Tessera <antra@tesserae.cc>
2025-01-05 14:20:29 -0500

    params horror

--- EasyLM/models/llama/llama_serve.py
+++ EasyLM/models/llama/llama_serve.py
@@ -302,7 +302,7 @@ class ModelServer(LMServer):
                 logging.info("Sharding parameters across mesh...")
             # Get combined sharding functions for both base and LoRA parameters
 
-            resolved_ps =  jax.tree_map(combined_ps, params)
+            resolved_ps =  jax.tree_map(lambda x: combined_ps, params)
             if jax.process_index() == 0:
                 logging.info(f"Resolved partition specs: {resolved_ps}")
 
