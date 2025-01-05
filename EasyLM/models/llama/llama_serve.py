f1481ca9390633721662635855613b024b3d4970
Antra Tessera <antra@tesserae.cc>
2025-01-04 22:47:49 -0500

    mesh change

--- EasyLM/models/llama/llama_serve.py
+++ EasyLM/models/llama/llama_serve.py
@@ -302,6 +302,7 @@ class ModelServer(LMServer):
             # Get combined sharding functions for both base and LoRA parameters
 
             #self.params = tree_apply(combined_shard_fns, params)
+            self.params = params
             self.sharded_rng = next_rng()
             logging.info(f"Mesh setup complete. Took {time.time() - mesh_start:.1f}s")
 
