4687e3079e98f1b9321ee5f4eb9e2d55f3bdcbff
Antra Tessera <antra@tesserae.cc>
2025-01-07 09:54:42 -0500

    worker_train repoint

--- train_tpu_pod.sh
+++ train_tpu_pod.sh
@@ -12,6 +12,6 @@ git push antra
 # Copy worker script and start training on all workers
 echo "Starting training on all workers..."
 gcloud compute tpus tpu-vm scp worker_train_lora.sh finetune-70b:~/worker_train_lora.sh --zone=us-central2-b --worker=all
-gcloud compute tpus tpu-vm ssh finetune-70b --zone=us-central2-b --worker=all --command="cd ~/EasyLM && git checkout origin/test_conv && git reset --hard HEAD && git pull && chmod +x ~/worker_train.sh && export WANDB_API_KEY='${WANDB_API_KEY}'  && export HF_TOKEN='${HF_TOKEN}' && ~/worker_train.sh"
+gcloud compute tpus tpu-vm ssh finetune-70b --zone=us-central2-b --worker=all --command="cd ~/EasyLM && git fetch && git checkout test_conv && git reset --hard HEAD && git pull && chmod +x ~/worker_train.sh && export WANDB_API_KEY='${WANDB_API_KEY}'  && export HF_TOKEN='${HF_TOKEN}' && ~/worker_train.sh"
 
 echo "Training deployment complete!"
