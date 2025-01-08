b1915ca27f9856c5ebda128fa2a6ab4bd7e270a2
Antra Tessera <antra@tesserae.cc>
2025-01-07 19:46:08 -0500

    trying train on main

--- worker_train.sh
+++ worker_train.sh
@@ -14,15 +14,15 @@ echo "HF_TOKEN is${HF_TOKEN:+ set}${HF_TOKEN:-" not set"}"
 cd ~/EasyLM && python -m EasyLM.models.llama.llama_train \
     --mesh_dim='2,-1,1' \
     --dtype='bf16' \
+    --seed 43 \
     --llama.base_model='llama31_8b' \
     --tokenizer="meta-llama/Meta-Llama-3.1-8B" \
     --load_checkpoint='params::/mnt/disk2/llama-3.1-8b' \
-    --train_dataset.huggingface_dataset.name="" \
-    --train_dataset.huggingface_dataset.path='tatsu-lab/alpaca' \
-    --train_dataset.huggingface_dataset.seq_length=4096 \
-    --train_dataset.huggingface_dataset.seq_length=2048 \
-    --train_dataset.huggingface_dataset.batch_size=64 \
-    --train_dataset.text_processor.fields='[instruction+input],output' \
+    --train_dataset.type='json' \
+    --train_dataset.text_processor.template="$(cat templates/borg_chat.yaml)" \
+    --train_dataset.json_dataset.path="/mnt/disk2/simulect_conversations.jsonl" \
+    --train_dataset.json_dataset.seq_length=1024 \
+    --train_dataset.json_dataset.batch_size=64 \
     --optimizer.type='adamw' \
     --optimizer.adamw_optimizer.lr=3e-4 \
     --optimizer.adamw_optimizer.end_lr=3e-5 \
@@ -34,6 +34,6 @@ cd ~/EasyLM && python -m EasyLM.models.llama.llama_train \
     --logger.online=true \
     --logger.project='levanter-sft' \
     --logger.entity='antra-cyborgism' \
-    --logger.output_dir='params::/mnt/disk2/easylm_chkp' \
+    --logger.output_dir='/mnt/disk2/easylm_chkp' \
     --checkpointing.save_min_step=1000 \
     --checkpointing.save_loss_threshold=0.8
