b948422700055e8269d617d9de04eae801652049
Antra Tessera <antra@tesserae.cc>
2025-01-10 12:38:57 -0500

    testing compilation cache

--- worker_train_70b.sh
+++ worker_train_70b.sh
@@ -41,4 +41,6 @@ cd ~/EasyLM && python -m EasyLM.models.llama.llama_lora_train \
     --logger.online=true \
     --logger.project='levanter-sft' \
     --logger.entity='antra-cyborgism' \
-    --logger.output_dir='gs://finetune70b/easylm_checkpoints_70b'
+    --logger.output_dir='gs://finetune70b/easylm_checkpoints_70b' \
+    --checkpointing.save_min_step=100 \
+    --checkpointing.save_loss_threshold=3
