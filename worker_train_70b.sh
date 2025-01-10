1b9bef52410b5fa54bd2f45fd75edb2dac825b4f
Antra Tessera <antra@tesserae.cc>
2025-01-10 14:28:50 -0500

    checkpoint change

--- worker_train_70b.sh
+++ worker_train_70b.sh
@@ -43,4 +43,5 @@ cd ~/EasyLM && python -m EasyLM.models.llama.llama_lora_train \
     --logger.entity='antra-cyborgism' \
     --logger.output_dir='gs://finetune70b/easylm_checkpoints_70b' \
     --checkpointing.save_min_step=100 \
-    --checkpointing.save_loss_threshold=3
+    --checkpointing.save_loss_threshold=3 \
+    --checkpointing.keep_recent=100
