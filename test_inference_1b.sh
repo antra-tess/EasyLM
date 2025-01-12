b89fadd443c70e100620dd735700b1019a799afd
Antra Tessera <antra@tesserae.cc>
2025-01-11 22:59:18 -0500

    tweaked inference

--- test_inference_1b.sh
+++ test_inference_1b.sh
@@ -30,20 +30,20 @@ cd ~/EasyLM
 python -m EasyLM.test_inference \
     --mesh_dim='1,-1,1' \
     --dtype='bf16' \
-    --llama.base_model='llama31_70b' \
+    --llama.base_model='llama32_1b' \
     --param_dtype='bf16' \
-    --tokenizer='meta-llama/Llama-3.1-70B' \
-    --load_checkpoint='base_params_unsharded::/mnt/disk2/llama-3.1-70b' \
+    --tokenizer='meta-llama/Llama-3.2-1B' \
+    --load_checkpoint='base_params_unsharded::/mnt/disk2/llama-3.2-1b.easylm' \
     --input_length=1024 \
     --seq_length=2048 \
     --do_sample=True \
     --top_k=150 \
     --top_p=0.99 \
     --lm_server.port=5009 \
-    --load_lora='base_params_unsharded::/mnt/disk2/easylm_chkp/0f24b03e4bec41f49c9b172a7adf2eea/checkpoint_1000/streaming_params' \
+    --load_lora='base_params_unsharded::/mnt/disk2/easylm_chkp/30bf244bf80c464d854f0c1addf55409/checkpoint_1000/streaming_params' \
     --lora_mode=True \
-    --llama.lora_rank=32 \
-    --llama.lora_alpha=64 \
+    --llama.lora_rank=16 \
+    --llama.lora_alpha=32 \
     --llama.lora_dropout=0.1 \
     --llama.lora_attn=true \
     --llama.lora_mlp=false \
