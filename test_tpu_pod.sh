b5996a122c8bfe0fda8b3aa341b0ea23fd5a046b
Antra Tessera <antra@tesserae.cc>
2025-01-11 22:20:08 -0500

    inference parametrized

--- test_tpu_pod.sh
+++ test_tpu_pod.sh
@@ -15,7 +15,7 @@ export SCRIPT=test_inference.sh
 
 # Copy script and run on all workers
 echo "Starting test on all workers..."
-gcloud compute tpus tpu-vm scp $SCRIPT finetune70b:~/$SCRIPT --zone=us-central2-b --worker=all
-gcloud compute tpus tpu-vm ssh finetune70b --zone=us-central2-b --worker=all --command="cd ~/EasyLM && git fetch && git reset --hard HEAD && git checkout main && git pull && chmod +x ~/$SCRIPT && export HF_TOKEN='${HF_TOKEN}' && ~/$SCRIPT"
+gcloud compute tpus tpu-vm scp $SCRIPT $INFER_NAME:~/$SCRIPT --zone=$INFER_ZONE --worker=all
+gcloud compute tpus tpu-vm ssh $INFER_NAME --zone=$INFER_ZONE --worker=all --command="cd ~/EasyLM && git fetch && git reset --hard HEAD && git checkout main && git pull && chmod +x ~/$SCRIPT && export HF_TOKEN='${HF_TOKEN}' && ~/$SCRIPT"
 
 echo "TPU pod test deployment complete!"
