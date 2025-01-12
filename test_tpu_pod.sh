9c56411d595f36e9d563e63fcb25900b26cb3db1
Antra Tessera <antra@tesserae.cc>
2025-01-11 22:56:12 -0500

    parametrize more scripts

--- test_tpu_pod.sh
+++ test_tpu_pod.sh
@@ -10,12 +10,9 @@ echo "Pushing local changes to repository..."
 git pull
 git push antra
 
-export SCRIPT=test_inference.sh
-
-
 # Copy script and run on all workers
 echo "Starting test on all workers..."
-gcloud compute tpus tpu-vm scp $SCRIPT $INFER_NAME:~/$SCRIPT --zone=$INFER_ZONE --worker=all
-gcloud compute tpus tpu-vm ssh $INFER_NAME --zone=$INFER_ZONE --worker=all --command="cd ~/EasyLM && git fetch && git reset --hard HEAD && git checkout main && git pull && chmod +x ~/$SCRIPT && export HF_TOKEN='${HF_TOKEN}' && ~/$SCRIPT"
+gcloud compute tpus tpu-vm scp $INFER_SCRIPT $INFER_NAME:~/$INFER_SCRIPT --zone=$INFER_ZONE --worker=all
+gcloud compute tpus tpu-vm ssh $INFER_NAME --zone=$INFER_ZONE --worker=all --command="cd ~/EasyLM && git fetch && git reset --hard HEAD && git checkout main && git pull && chmod +x ~/$INFER_SCRIPT && export HF_TOKEN='${HF_TOKEN}' && ~/$INFER_SCRIPT"
 
 echo "TPU pod test deployment complete!"
